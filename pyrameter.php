c3ec6099dd0c0a63b159622bdf64c900d37a4998
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-15 03:10:17 +0700

    use defaults

--- pyrameter.php
+++ pyrameter.php
@@ -5,17 +5,7 @@ declare(strict_types=1);
 use Pyrameter\Config\PyrameterConfig;
 use Pyrameter\TestKind;
 
-return PyrameterConfig::create()
-    ->usesClass(PDO::class, TestKind::Integration)
-    ->usesClass(mysqli::class, TestKind::Integration)
-    ->usesNamespace('Doctrine\DBAL\\', TestKind::Integration)
-    ->usesNamespace('Doctrine\ORM\\', TestKind::Integration)
-    ->usesNamespace('Doctrine\ODM\\', TestKind::Integration)
-    ->usesNamespace('Redis\\', TestKind::Integration)
-    ->usesNamespace('Predis\\', TestKind::Integration)
-    ->usesNamespace('Symfony\Bundle\FrameworkBundle\Test\\', TestKind::Functional)
-    ->usesNamespace('Symfony\Component\Panther\\', TestKind::E2E)
-    ->usesNamespace('Facebook\WebDriver\\', TestKind::E2E)
+return PyrameterConfig::defaults()
     ->targetShape(
         unit: ['min' => 60],
         functional: ['max' => 20],
